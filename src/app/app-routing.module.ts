a3938b27879371fb4e32ac141086268cbd10afe9
Sara Hanks <shanksalot78@gmail.com>
2020-05-19 21:19:09 -0400

    day3 of 100

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { NewProjectFormComponent } from './new-project-form/new-project-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +7,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'new', component: NewProjectFormComponent
+  },
+  {
+    path: '', component: SignUpPageComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewProjectFormComponent } from './new-project-form/new-project-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 /// Material module imports for Angular Material
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewProjectFormComponent
+    NewProjectFormComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
